88da2dbb6c277cd0ea57401f1b7b46c2d06efd4b
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-06-02 15:30:03 +0200

    aktualizacja folderow

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
-from app import app
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from app import app
 def test_home():
     app.testing = True
     client = app.test_client()
